68c429989e9158e04d13372ffcd76f5a5c204858
Anastasia <nastya.k.r@mail.ru>
2017-12-05 16:46:41 +0300

    for michailo

--- src/reducers/login.js
+++ src/reducers/login.js
@@ -4,6 +4,7 @@ const socialNetworks = [{
   to: "https://oauth.vk.com/authorize?client_id="
   + 6278299 + "&display=popup&redirect_uri="
   + "https://nkwalker.herokuapp.com/"
+  // + "http://localhost:3000/"
   + "&scope=offline,friends,photos&response_type=token&v=5.69",
   src: "img/vk.svg"
 }, {
@@ -25,7 +26,8 @@ const socialNetworks = [{
 
 const entryButton = { key: 0,
   name: "Войти на сайт",
-  to: "http://localhost:3000/account/",
+  to: "https://nkwalker.herokuapp.com/account/",
+  // to: "http://localhost:3000/account/",
   src: "img/entry.svg"};
 
 export default function login(state = {
